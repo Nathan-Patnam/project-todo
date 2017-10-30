1951f08e7886de526726b4c19211f17deb3a2d94
dylan <haddende@email.wofford.edu>
2017-10-30 13:53:14 -0400

    Feature 2 finish

--- src/main/java/keywords/Feature02.java
+++ src/main/java/keywords/Feature02.java
@@ -4,17 +4,28 @@ import edu.wofford.*;
 
 public class Feature02 {
 	private ArgumentParser argCheck;
+	private String helpmessage;
 
     public void startProgramWithArguments(String[] args) {
         argCheck = new ArgumentParser("VolumeCalculator", "Calculate the volume of a box.");
-        argCheck.addArg("length", "the length of the box");
-        argCheck.addArg("width", "the width of the box");
-        argCheck.addArg("height", "the height of the box");
-        argCheck.parse(args);
+        argCheck.addArg("length", "the length of the box (float)");
+        argCheck.addArg("width", "the width of the box (float)");
+        argCheck.addArg("height", "the height of the box (float)");
+        try{argCheck.parse(args);
+				}
+				catch(HelpException e){
+					helpmessage = e.getMessage();
+					System.out.println("print message below");
+
+					System.out.println(helpmessage);
+
+					System.out.println("bye bye");
+
+				}
 }
 
 public String getProgramOutput() {
-	return argCheck.getHelpMessage();
+	return helpmessage;
 }
 
 
