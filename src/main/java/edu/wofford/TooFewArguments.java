22a86002514bc2b498e8c9b51c7caf9f16f3c0c9
Jacey <sohmjc@email.wofford.edu>
2017-12-01 13:28:49 -0500

    Added some documentation for the Exception classes

--- src/main/java/edu/wofford/TooFewArguments.java
+++ src/main/java/edu/wofford/TooFewArguments.java
@@ -1,6 +1,12 @@
 package edu.wofford;
 
 class TooFewArguments extends RuntimeException {
+  /**
+  * Constructor for a TooFewArguments exception that is thrown when too few arguments are provided in the user 
+  * input. The message should include helpful usage information about the missing arguments.
+  * @param  message, a String value that is the message that should contain usage information
+  * @return a TooFewArguments exception 
+  */
   public TooFewArguments(String message) {
     super(message);
   }
